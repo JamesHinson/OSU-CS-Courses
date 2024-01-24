8cd4e37a179c8f16b0630f031b648bf766ed0ea7
James Hinson <jamesmasonhinson@gmail.com>
2024-01-24 15:13:40 -0800

    Added extra functionality to assignment1.asm

--- 271/assignment1/assignment1.asm
+++ 271/assignment1/assignment1.asm
@@ -1,17 +1,15 @@
-TITLE Assignment 1     (assignment1.asm)
+TITLE Assignment1     (assignment1.asm)
 
 ; Author(s): James Hinson
 ; Course Number: CS 271
 ; Date: 1/24/2024
-; Description:  The first assignment for CS 271 - Computer Architecture and Assembly Language
-; 				Calculates the area, perimeter, and linear feet of wooden planks needed to
-; 				fence a pasture of a size given by the user.
+; Description:  The first assignment for CS 271 - Computer Architecture and Assembly Language.
+; 				Calculates the area, perimeter, and number of wooden rails in linear feet needed
+; 				to fence a pasture of a size given by the user.
 
 INCLUDE Irvine32.inc
 
 ; (insert constant definitions here)
-PLANK_LENGTH = 6
-
 
 .data
 
@@ -20,14 +18,14 @@ PLANK_LENGTH = 6
 	userName	BYTE		50 DUP(0)
 	intro1		BYTE		'This program finds the area, perimeter, and number of 1x6" planks needed ', 0
 	intro2		BYTE		"to make a fence around a pasture of a given length.", 0
-	promptName	BYTE		"What is your name? Press enter when done: ", 0
+	promptName	BYTE		"What is your name? Press enter when done : ", 0
 	promptLen	BYTE		"Enter the length of your pasture (in feet) : ", 0
 	promptWid	BYTE		"Enter the width of your pasture (in feet) : ", 0
 	promptPlank	BYTE		"Enter the linear feet of wood planks available : ", 0
 	areaOutput	BYTE		"The area of the pasture is : ", 0
 	permOutput	BYTE		"The perimeter of the pasture is : ", 0
 	plankOut1	BYTE		"You have enough wood for ", 0
-	plankOut2	BYTE		" and an extra ", 0
+	plankOut2	BYTE		" rails and an extra ", 0
 	plankOut3	BYTE		' linear feet of 1x6" planks', 0
 	loopPrompt	BYTE		"Would you like to do another calculation? (0=NO 1=YES) : ", 0
 	goodBye1	BYTE		"Goodbye ", 0
@@ -97,12 +95,12 @@ PLANK_LENGTH = 6
 	mul 	ebx         ; Multiply the sum by 2 to get perimeter
 	mov 	perimeter, eax
 
-	; Calculate the number of planks
-	mov 	eax, perimeter
-	mov 	ebx, PLANK_LENGTH
-	div 	ebx          	; Divide EDX:EAX by EBX, result in EAX (quotient), EDX (remainder)
-	mov 	numPlanks, eax  ; Store the number of planks in numPlanks
-	mov 	remainder, edx	; Store the remainder in a separate variable
+	; Calculate the number of rails
+	mov 	eax, inputPlank
+	mov 	ebx, perimeter
+	div 	ebx             ; Divide eax (promptPlank) by ebx (perimeter)
+	mov 	numPlanks, eax  ; Store the number of rails in numPlanks
+	mov 	remainder, edx  ; Store the remainder of planks in remainder
 
 	; Display the area
 	mov 	edx, OFFSET areaOutput
@@ -114,16 +112,19 @@ PLANK_LENGTH = 6
 	call	WriteString
 	call	Crlf
 
-	; Display the number of possible rails
+	; Display the number of rails
 	mov 	edx, OFFSET plankOut1
 	call	WriteString
 	mov 	edx, numPlanks
 	call	WriteInt
 	mov 	edx, OFFSET plankOut2
 	call	WriteString
+
+	; Display the amount of extra planking
+	mov 	edx, OFFSET plankOut3
+	call	WriteString
 	mov 	edx, remainder
 	call	WriteInt
-	call	Crlf
 	call	Crlf
 
 	; Ask if user wants to do another calculation
@@ -137,7 +138,7 @@ PLANK_LENGTH = 6
 	cmp 	inputLoop, 1
 	je  	main
 
-	; Say goodbye to user
+	; Say goodbye to the user
 	mov 	edx, OFFSET goodBye1
 	call	WriteString
 	mov 	edx, OFFSET userName
